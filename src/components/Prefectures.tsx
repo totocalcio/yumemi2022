be54173b587d65a93678a402d65dc0b37316d56a
totocalcio <totohyper@yahoo.co.jp>
2022-04-24 10:38:11 +0900

    人口構成取得処理実装

--- src/components/Prefectures.tsx
+++ src/components/Prefectures.tsx
@@ -1,5 +1,16 @@
+import axios, { AxiosResponse, AxiosError } from 'axios'
 import * as React from 'react'
 import styled from 'styled-components'
+
+interface ResasResponse {
+  statusCode: string
+  description: string
+  message: string | null
+  result: {
+    prefCode: number
+    prefName: string
+  }[]
+}
 
 type Props = {
   prefectures: {
@@ -15,10 +26,41 @@ const List = styled.ul`
   list-style: none;
 `
 
+const getPopulationURL =
+  'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear'
+
 export const Prefectures: React.FC<Props> = (props) => {
+  const [post, setPost] = React.useState<ResasResponse>(null!)
+  const [error, setError] = React.useState<AxiosError>(null!)
+  const getApi = (id: number) => {
+    console.log(1)
+    if (process.env.REACT_APP_RESAS_API_KEY) {
+      console.log(`${getPopulationURL}?cityCode=-&prefCode=${id}`)
+      axios
+        .get(
+          'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=11',
+          {
+            headers: { 'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY },
+          }
+        )
+        .then((result: AxiosResponse<ResasResponse>) => {
+          setPost(result.data)
+        })
+        .catch((error: AxiosError<{ error: string }>) => {
+          setError(error)
+        })
+    }
+    if (error) {
+      console.log(error)
+    } else {
+      console.log(post)
+    }
+  }
+
   const [checkedState, setCheckedState] = React.useState(props.prefectures)
 
   const handleCheckedState = (position: number) => {
+    getApi(position)
     const updateItem = (index: number) => {
       checkedState[index].checked = !checkedState[index].checked
       return checkedState[index]
