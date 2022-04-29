f51d04334371e52378a67066dca317c0351b0247
totocalcio <totohyper@yahoo.co.jp>
2022-04-29 14:05:02 +0900

    グラフ(Highcharts)実装

--- src/components/Prefectures.tsx
+++ src/components/Prefectures.tsx
@@ -1,16 +1,29 @@
 import axios, { AxiosResponse, AxiosError } from 'axios'
 import * as React from 'react'
 import styled from 'styled-components'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 
-interface ResasResponse {
+interface ResasResponse<T> {
   statusCode: string
   description: string
   message: string | null
-  result: {
-    prefCode: number
-    prefName: string
+  result: T
+}
+
+type Result = {
+  boundaryYear: number
+  data: {
+    label: string
+    data: {
+      year: number
+      value: number
+      rate?: number
+    }[]
   }[]
 }
+
+type Population = ResasResponse<Result>
 
 type Props = {
   prefectures: {
@@ -18,6 +31,12 @@ type Props = {
     prefName: string
     checked?: boolean
   }[]
+}
+
+type Series = {
+  type: 'line'
+  name: string
+  data: number[]
 }
 
 const List = styled.ul`
@@ -30,12 +49,63 @@ const getPopulationURL =
   'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear'
 
 export const Prefectures: React.FC<Props> = (props) => {
-  const [post, setPost] = React.useState<ResasResponse>(null!)
-  const [error, setError] = React.useState<AxiosError>(null!)
+  const [post, setPost] = React.useState<Population>(null!)
+  const [checkedState, setCheckedState] = React.useState(props.prefectures)
+  const [series, setSeries] = React.useState<Series[]>([])
+  const [position, setPosition] = React.useState(0)
+  const chartComponentRef = React.useRef<HighchartsReact.RefObject>(null)
+
+  const categories = ['1970', '1980', '1990', '2000', '2010', '2020']
+
+  const options: Highcharts.Options = {
+    title: {
+      text: '総人口推移',
+    },
+    xAxis: {
+      title: {
+        text: '年度',
+      },
+      categories,
+    },
+    yAxis: {
+      title: {
+        text: '人口数',
+      },
+    },
+    series,
+  }
+
+  React.useEffect(() => {
+    if (post) {
+      if (checkedState[position].checked === true) {
+        setSeries([...series, dataset2(checkedState[position].prefName)])
+      } else {
+        setSeries(
+          series.filter((item) => item.name !== checkedState[position].prefName)
+        )
+      }
+    }
+  }, [post])
+  const data = () => {
+    const filterArr = post.result.data.find((elm) => elm.label === '総人口')
+    if (!filterArr) return []
+    const tempArr = filterArr.data.filter(
+      (elm) =>
+        Number(categories[0]) <= elm.year &&
+        elm.year <= Number(categories[categories.length - 1]) &&
+        elm.year.toString().slice(-1) === '0'
+    )
+    return tempArr.map((elm) => elm.value)
+  }
+
+  const dataset2 = (name: string): Series => ({
+    type: 'line',
+    name,
+    data: data(),
+  })
+
   const getApi = (id: number) => {
-    console.log(1)
     if (process.env.REACT_APP_RESAS_API_KEY) {
-      console.log(`${getPopulationURL}?cityCode=-&prefCode=${id}`)
       axios
         .get(
           'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=11',
@@ -43,24 +113,20 @@ export const Prefectures: React.FC<Props> = (props) => {
             headers: { 'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY },
           }
         )
-        .then((result: AxiosResponse<ResasResponse>) => {
+        .then((result: AxiosResponse<Population>) => {
           setPost(result.data)
         })
         .catch((error: AxiosError<{ error: string }>) => {
-          setError(error)
+          console.error(error)
         })
-    }
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(post)
     }
   }
 
-  const [checkedState, setCheckedState] = React.useState(props.prefectures)
-
   const handleCheckedState = (position: number) => {
+    setPosition(position)
     getApi(position)
+
+    //checkボックス更新処理
     const updateItem = (index: number) => {
       checkedState[index].checked = !checkedState[index].checked
       return checkedState[index]
@@ -86,5 +152,15 @@ export const Prefectures: React.FC<Props> = (props) => {
     </li>
   ))
 
-  return <List>{mapList}</List>
+  return (
+    <>
+      <List>{mapList}</List>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        ref={chartComponentRef}
+        {...props}
+      />
+    </>
+  )
 }
